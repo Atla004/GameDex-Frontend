deb9adf9a6df9f0a114ff2b9cadc6fe52e29fe9a
Atla004 <andrese.g.v.13579@gmail.com>
2024-11-08 21:10:24 -0400

    refactor: update import paths for GameDetail and ReviewSection components; remove unused Root component

--- app/GameScreen.tsx
+++ app/GameScreen.tsx
@@ -9,8 +9,8 @@ import {
   Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { GameDetail } from "../components/GameComponents/GameDetail";
-import { ReviewSection } from "../components/GameComponents/ReviewSection";
+import { GameDetail } from "@/components/GameComponents/GameDetail";
+import { ReviewSection } from "@/components/GameComponents/ReviewSection";
 import { PokeBallRating } from "@/components/GameComponents/PokeballRating";
 import { StatusBar } from "react-native";
 import { router } from "expo-router";
